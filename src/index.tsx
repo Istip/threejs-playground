ccb9628ba4ff4519f38336a355caab58f04f9678
Pasztor Isti <isticsek@gmail.com>
2024-01-26 17:15:08 +0200

    update main canvas

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
+import Scene from "./components/Scene";
+import { Suspense } from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={null}>
+    <Canvas
+      orthographic
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 1000,
+        zoom: 120,
+        position: [0, 0, 6],
+      }}
+    >
+      <Scene />
+    </Canvas>
+  </Suspense>
 );
 
 reportWebVitals();
