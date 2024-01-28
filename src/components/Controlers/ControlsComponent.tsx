e475a325a05f15dc1a52b88fe6ae50f895bf1e11
Pasztor Isti <isticsek@gmail.com>
2024-01-28 16:19:02 +0200

    add camera controller for the main test file

---
+++ src/components/Controlers/ControlsComponent.tsx
@@ -0,0 +1,11 @@
+import CameraController from "./CameraControls";
+
+const ControlsComponent = () => {
+  return (
+    <>
+      <CameraController />
+    </>
+  );
+};
+
+export default ControlsComponent;
