3c3c7495a78306714572c672064527b01e80197d
Pasztor Isti <isticsek@gmail.com>
2024-02-15 17:53:53 +0200

    create custom glass effect

--- src/components/GlassTest.tsx
+++ src/components/GlassTest.tsx
@@ -1,53 +1,79 @@
-import {
-  Environment,
-  MeshTransmissionMaterial,
-  OrbitControls,
-} from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import { Environment, MeshTransmissionMaterial } from "@react-three/drei";
+import { folder, useControls } from "leva";
 import Texts from "./Texts";
-import { useControls } from "leva";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const GlassTest = () => {
+  const [hover, setHover] = useState(false);
+  const ref = useRef(null as any);
+
+  const { lerp } = THREE.MathUtils;
+
+  useFrame(({ pointer }) => {
+    // POSITION CHANGE
+    ref.current.position.x = lerp(ref.current.position.x, pointer.x, 1);
+    ref.current.position.y = lerp(ref.current.position.y, pointer.y, 1);
+
+    // SCALE CHANGE
+    ref.current.scale.x = lerp(ref.current.scale.x, hover ? 3 : 1, 0.1);
+    ref.current.scale.y = lerp(ref.current.scale.y, hover ? 3 : 1, 0.1);
+  });
+
+  // LEVA CONTROLS
   const {
     distortionScale,
     temporalDistortion,
     thickness,
     ior,
     chromaticAberration,
+    roughness,
+    anisotropicBlur,
+    transmission,
+    backsided,
   } = useControls("GLASS", {
-    distortionScale: { value: 0.5, min: 0, max: 10, step: 0.05 },
-    temporalDistortion: { value: 0.5, min: 0, max: 10, step: 0.05 },
-    thickness: { value: 0.5, min: 0, max: 10, step: 0.05 },
-    ior: { value: 0.5, min: 0, max: 10, step: 0.05 },
-    chromaticAberration: { value: 0.5, min: 0, max: 10, step: 0.05 },
+    DEFAULT: folder({
+      distortionScale: { value: 0.5, min: 0, max: 10, step: 0.05 },
+      temporalDistortion: { value: 0.5, min: 0, max: 10, step: 0.05 },
+    }),
+    BASIC: folder({
+      thickness: { value: 0.85, min: 0, max: 10, step: 0.05 },
+      ior: { value: 2, min: 0, max: 10, step: 0.01 },
+      chromaticAberration: { value: 0.5, min: 0, max: 10, step: 0.05 },
+    }),
+    STRANGE: folder({
+      roughness: { value: 0, min: 0, max: 10, step: 0.01 },
+      anisotropicBlur: { value: 0, min: 0, max: 10, step: 0.01 },
+      transmission: { value: 1, min: 0, max: 10, step: 0.01 },
+      backsided: false,
+    }),
   });
 
   return (
     <>
-      <Canvas
-        orthographic
-        camera={{
-          fov: 75,
-          near: 0.1,
-          far: 50,
-          position: [0, 0, 6],
-          zoom: 200,
-        }}
+      <Environment preset="city" />
+      <Texts />
+      <mesh
+        ref={ref}
+        onPointerOver={() => setHover(true)}
+        onPointerOut={() => setHover(false)}
+        position={[0, 0, 2]}
+        scale={2}
       >
-        <OrbitControls />
-        <Environment preset="sunset" />
-        <Texts />
-        <mesh position={[0, 0, 3]} scale={1}>
-          <circleGeometry args={[1, 64, 64]} />
-          <MeshTransmissionMaterial
-            distortionScale={distortionScale}
-            temporalDistortion={temporalDistortion}
-            thickness={thickness}
-            ior={ior}
-            chromaticAberration={chromaticAberration}
-          />
-        </mesh>
-      </Canvas>
+        <circleGeometry args={[1, 64, 64]} />
+        <MeshTransmissionMaterial
+          distortionScale={distortionScale}
+          temporalDistortion={temporalDistortion}
+          thickness={thickness}
+          ior={ior}
+          chromaticAberration={chromaticAberration}
+          roughness={roughness}
+          anisotropicBlur={anisotropicBlur}
+          transmission={transmission}
+          backside={backsided}
+        />
+      </mesh>
     </>
   );
 };
